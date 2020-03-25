42d763147c46597069d18779b7668d687f3e8623
Aya Hassan <40079035+ayahassan1@users.noreply.github.com>
2020-03-25 07:24:50 +0200

    Update game.js

--- Let's-Think(Beta)/Front-end/src/assets/js/game.js
+++ Let's-Think(Beta)/Front-end/src/assets/js/game.js
@@ -1,19 +1,23 @@
 var result1 , result2, score=0, x , y, answer;
     var arr=["+" , "-"];
     var arr2 =[ "*", "/"];
+    var l2 =0;
+    var r1=100 , r2=50 ,r3=10 , m1=1 , m2=1 , m3=1;
     var operator1 = arr[Math.floor(Math.random() * arr.length)];
     var operator2 = arr2[Math.floor(Math.random() * arr2.length)];
-    var a = Math.floor(Math.random() * 100)+1;
-    var b = Math.floor(Math.random() * 50)+1;
-    var c = Math.floor(Math.random() * 10)+1;
+    var a = Math.floor(Math.random() * (r1-m1+1))+m1;
+    var b = Math.floor(Math.random() * (r2-m2+1))+m2;
+    var c = Math.floor(Math.random() * (r3-m3 +1))+m3;
     var res = a +"  "+ operator1 +"  "+ b + "  "+ operator2 +"  "+c;
-
     function onclick1(){
       
         switch(operator2){
             case "/":
             { 
-            result1 = Math.floor(b/c);
+              
+            result1 = (b/c);
+              
+             
             break;
             }
             case "*":
@@ -36,25 +40,47 @@ var result1 , result2, score=0, x , y, answer;
                 }
                 
         }
-        result1 = Math.floor(result1);
+        if((result1*10) % 10 !=0){
+        result1 = result1.toFixed(1);
+        }
         answer= (document.getElementById("ans")).value ;
         if(answer == ""){
             alert("please Enter your result");
         }
+        
         else if(answer==result1){
             score +=15;
-        
+            if(score >=60){
+                l2=60;
+            }
+            else if(score >= 120){
+                l2 = 120;
+            }
+            if(l2==60){
+                r1 =1000;
+                r2=100;
+                r3 = 20;
+                m1 = 100, m2 = 50, m3 =10;
+            }
+            else if(l2 == 120){
+                r1 =10000;
+                r2=200;
+                r3 = 50;
+                m1 = 1000, m2 = 100, m3 =20;
+            }
         document.getElementById("score").innerHTML=score;
         document.getElementById("ans").innerHTML="";
         operator1 = arr[Math.floor(Math.random() * arr.length)];
         operator2 = arr2[Math.floor(Math.random() * arr2.length)];
-        a = Math.floor(Math.random() * 100)+1;
-        b = Math.floor(Math.random() * 50)+1;
-        c = Math.floor(Math.random() * 10)+1;
+         a = Math.floor(Math.random() * (r1-m1)+m1);
+        b = Math.floor(Math.random() * (r2-m2)+m2);
+        c = Math.floor(Math.random() * (r3-m3)+m3);
         res = a +"  "+ operator1 +"  "+ b + "  "+ operator2 +"  "+c;
            myfun();
         }
-        else {
+        else  {
+            
+           
          score -=15;
          var cont =window.confirm("oops! \n the correct ansewer is : "+ result1 +"\n your score now = " +score +"\n\n do you want to continue ?");
         if(cont == true){
@@ -62,9 +88,9 @@ var result1 , result2, score=0, x , y, answer;
         document.getElementById("ans").innerHTML="";
         operator1 = arr[Math.floor(Math.random() * arr.length)];
         operator2 = arr2[Math.floor(Math.random() * arr2.length)];
-        a = Math.floor(Math.random() * 100)+1;
-        b = Math.floor(Math.random() * 50)+1;
-        c = Math.floor(Math.random() * 10)+1;
+         a = Math.floor(Math.random() * (r1-m1)+m1);
+         b = Math.floor(Math.random() * (r2-m2)+m2);
+          c = Math.floor(Math.random() * (r3-m3)+m3);
         res = a +"  "+ operator1 +"  "+ b + "  "+ operator2 +"  "+c;
            myfun();
         }
@@ -74,7 +100,8 @@ var result1 , result2, score=0, x , y, answer;
         }
         
     }
-    
+ 
+
 function myfun(){
      document.getElementById("equation").innerHTML= res;
      document.getElementById("bt").innerHTML = " <button id='btn1' onclick=onclick1() > <i class='fab fa-telegram-plane'></i> </button>" ;
